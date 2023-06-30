62a5e8082be8add0a1747a52b76939b9eb2feaa0
cobragh <lucas_alvesf09@outlook.com>
2023-06-29 21:37:42 -0300

    first commit

---
+++ server/controllers/auth.js
@@ -0,0 +1,11 @@
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import user from "../models/User";
+
+// REGISTER USER
+export const register = async (req, res) => {
+  try {
+  } catch (e) {
+    console.log(e);
+  }
+};
